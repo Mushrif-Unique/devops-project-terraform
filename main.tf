1fb9fab76f13665f6061756503f25221490623e2
Mushrif-Unique <mushrifjr3298@gmail.com>
2026-08-12 19:18:04 +0530

    feat: provision AWS VPC networking with Terraform

--- main.tf
+++ main.tf
@@ -1,3 +1,68 @@
-resource "aws_s3_bucket" "terraform_demo" {
-  bucket = "mushrif-devops-project2-terraform-2026"
+# ----------------------------------------
+# VPC
+# ----------------------------------------
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "devops-project2-vpc"
+  }
+}
+
+
+# ----------------------------------------
+# Public Subnet
+# ----------------------------------------
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-southeast-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "devops-project2-public-subnet"
+  }
+}
+
+
+# ----------------------------------------
+# Internet Gateway
+# ----------------------------------------
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "devops-project2-igw"
+  }
+}
+
+
+# ----------------------------------------
+# Public Route Table
+# ----------------------------------------
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "devops-project2-public-route-table"
+  }
+}
+
+
+# ----------------------------------------
+# Route Table Association
+# ----------------------------------------
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
